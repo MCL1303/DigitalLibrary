46b004e0d750cb89574603b980ff36f818fba0de
igor <igortarakanov144999usa@gmail.com>
2015-08-19 20:20:20 +0300

    [+] auto-resizer

--- resizer.py
+++ resizer.py
@@ -6,18 +6,25 @@ import PIL.Image
 
 def load_config():
     config = configparser.ConfigParser()
-    config.read('config')
-    return config['Resizer']
+    config.read("config")
+    return config["Resizer"]
 
 
 def main():
     config = load_config()
-    img = PIL.Image.open(config["path"])
-    height = int(config["height"])
+    
+    img = PIL.Image.open(config["/home/igor/Coding/Library/image.jpg"])
+    height = 320
     width = int(img.size[0] * (height / img.size[1]))
     img = img.resize((width, height), PIL.Image.ANTIALIAS)
-    img.save(config["path"])
+    img.save(config["/home/igor/Coding/Library/static/book/small-covers" + config["name"] + ".jpg"])
+
+    img = PIL.Image.open(config["/home/igor/Coding/Library/image.jpg"])
+    height = 480
+    width = int(img.size[0] * (height / img.size[1]))
+    img = img.resize((width, height), PIL.Image.ANTIALIAS)
+    img.save(config["/home/igor/Coding/Library/static/book/large-covers" + config["name"] + ".jpg"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
