35457688de2b62449699d488064d29e058964f4e
Igor Tarakanov <igortarakanov144999usa@gmail.com>
2015-08-26 23:11:42 +0300

    [+] backup.py, path to cron

--- hour_backup.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-
-base = pymongo.MongoClient()
-
-base.twoHour = base.oneHour
-base.oneHour = base.digital_library
--- week_backup.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-
-base = pymongo.MongoClient()
-
-base.twoWeek = base.oneWeek
-base.oneWeek = base.digital_library
