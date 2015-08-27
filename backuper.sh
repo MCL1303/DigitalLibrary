8ef94bf35d743f990376e209dd6d19d8f1851a51
Igor Tarakanov <igortarakanov144999usa@gmail.com>
2015-08-27 15:57:39 +0300

    [m] var/backups/digital_library

--- backuper.sh
+++ backuper.sh
@@ -1,17 +1,17 @@
 if [[ "$1" == "h" ]]; then
-	rm /var/digital_library/twoHour -rf
-	mv /var/digital_library/oneHour /var/digital_library/twoHour
-	mongodump -d digital_library -o /var/digital_library/oneHour
+	rm /var/backups/digital_library/twoHour -rf
+	mv /var/backups/digital_library/oneHour /var/backups/digital_library/twoHour
+	mongodump -d digital_library -o /var/backups/digital_library/oneHour
 fi
 
 if [[ "$1" == "d" ]]; then
-	rm /var/digital_library/twoDay -rf
-	mv /var/digital_library/oneDay /var/digital_library/twoDay
-	mongodump -d digital_library -o /var/digital_library/oneDay
+	rm /var/backups/digital_library/twoDay -rf
+	mv /var/backups/digital_library/oneDay /var/backups/digital_library/twoDay
+	mongodump -d digital_library -o /var/backups/digital_library/oneDay
 fi
 
 if [[ "$1" == "w" ]]; then
-	rm /var/digital_library/twoWeek -rf
-	mv /var/digital_library/oneWeek /var/digital_library/twoWeek
-	mongodump -d digital_library -o /var/digital_library/oneWeek
+	rm /var/backups/digital_library/twoWeek -rf
+	mv /var/backups/digital_library/oneWeek /var/backups/digital_library/twoWeek
+	mongodump -d digital_library -o /var/backups/digital_library/oneWeek
 fi
