b989f1003e1701af9ebff6a1f3dcfa897f0465a1
Igor Tarakanov <igortarakanov144999usa@gmail.com>
2015-08-27 15:55:51 +0300

    [+] root cron

--- backuper.sh
+++ backuper.sh
@@ -1,17 +1,17 @@
 if [[ "$1" == "h" ]]; then
-	rm ~/digital_library/backups/twoHour -rf
-	mv ~/digital_library/backups/oneHour ~/digital_library/backups/twoHour
-	mongodump -d digital_library -o ~/digital_library/backups/oneHour
+	rm /var/digital_library/twoHour -rf
+	mv /var/digital_library/oneHour /var/digital_library/twoHour
+	mongodump -d digital_library -o /var/digital_library/oneHour
 fi
 
 if [[ "$1" == "d" ]]; then
-	rm ~/digital_library/backups/twoDay -rf
-	mv ~/digital_library/backups/oneDay ~/digital_library/backups/twoDay
-	mongodump -d digital_library -o ~/digital_library/backups/oneDay
+	rm /var/digital_library/twoDay -rf
+	mv /var/digital_library/oneDay /var/digital_library/twoDay
+	mongodump -d digital_library -o /var/digital_library/oneDay
 fi
 
 if [[ "$1" == "w" ]]; then
-	rm ~/digital_library/backups/twoWeek -rf
-	mv ~/digital_library/backups/oneWeek ~/digital_library/backups/twoWeek
-	mongodump -d digital_library -o ~/digital_library/backups/oneWeek
+	rm /var/digital_library/twoWeek -rf
+	mv /var/digital_library/oneWeek /var/digital_library/twoWeek
+	mongodump -d digital_library -o /var/digital_library/oneWeek
 fi
