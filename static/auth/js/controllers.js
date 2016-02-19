ee6552b43264f8782b328e57176083e093c2fee7
Igor Tarakanov <igortarakanov144999usa@gmail.com>
2016-02-19 21:23:52 +0300

    [+] password checker

--- static/auth/js/controllers.js
+++ static/auth/js/controllers.js
@@ -19,4 +19,53 @@ DigitalLibraryControllers.controller('SigninCtrl', ['$scope',
 DigitalLibraryControllers.controller('SignupCtrl', ['$scope',
 	function($scope) {
 		document.title = 'Регистрация';
+		$scope.progress = 0;
+		String.prototype.replaceAll = function(search, replacement) {
+			var target = this;
+			return target.replace(new RegExp(search, 'g'), replacement);
+		};
+		$('#password').on('input', function() {
+			var password = $("#password")[0].value
+			$scope.progress = 0;
+			if(password.length < 8) {
+				$scope.progress = password.length * 4;
+				return;
+			} else {
+				$scope.progress = 10;
+			}
+			var i;
+			for(i = 0; i < 10; i++) {
+				if(password.search(i.toString()) != -1) {
+					$scope.progress += 20;
+					break;
+				}
+			}
+			for(i = 0; i < 25; i++) {
+				if(password.search(String.fromCharCode(65 + i)) != -1) {
+					$scope.progress += 20;
+					break;
+				}
+			}
+			for(i = 0; i < 25; i++) {
+				if(password.search(String.fromCharCode(97 + i)) != -1) {
+					$scope.progress += 20;
+					break;
+				}
+			}
+
+
+			for(i = 0; i < 10; i++) {
+				password = password.replaceAll(i.toString(), '')
+			}
+			for(i = 0; i < 25; i++) {
+				password = password.replaceAll(String.fromCharCode(65 + i), '')
+			}
+			for(i = 0; i < 25; i++) {
+				password = password.replaceAll(String.fromCharCode(97 + i), '')
+			}
+			if(password != '') {
+				$scope.progress = 100;
+				console.log(password);
+			}
+		});
 }]);
